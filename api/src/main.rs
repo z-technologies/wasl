1a4aeeb40d0d9eccd859525daa2f799f4e61edf0
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-04 00:19:38 +0300

    Setup a simple HTTP server

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod routes;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listen_endpoint = format!("{}:{}", config::LISTEN_ADDRESS, config::LISTEN_PORT);
+
+    HttpServer::new(|| App::new().configure(config::configure_server))
+        .bind(listen_endpoint)?
+        .run()
+        .await
 }
