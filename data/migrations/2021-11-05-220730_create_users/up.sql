6a3c5d60f553679b3d32022eecd41893f1d69c45
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-06 01:35:29 +0300

    Add a migration to create users table

---
+++ data/migrations/2021-11-05-220730_create_users/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id            SERIAL       PRIMARY KEY,
+
+    username      VARCHAR(32)  NOT NULL,
+    password_hash VARCHAR(64)  NULL,
+    password_salt VARCHAR(64)  NULL,
+
+    email         VARCHAR(64)  NOT NULL,
+    is_active     BOOLEAN      NOT NULL DEFAULT FALSE,
+
+    first_name    VARCHAR(32)  NULL,
+    last_name     VARCHAR(32)  NULL,
+    profile_photo VARCHAR(256) NULL
+)
