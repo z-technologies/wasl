73776f784ef88cd79defbe1283449c3684328674
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-06 19:25:50 +0300

    Implement Display on the error type of this crate

--- data/src/result.rs
+++ data/src/result.rs
@@ -1,8 +1,20 @@
 use diesel;
 
+use std::fmt;
+
+#[derive(Debug)]
 pub enum DataError {
     InternalError(diesel::result::Error),
     NotFound,
 }
 
 pub type Result<T> = std::result::Result<T, DataError>;
+
+impl fmt::Display for DataError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DataError::InternalError(err) => write!(f, "{}", err),
+            DataError::NotFound => write!(f, "no such item"),
+        }
+    }
+}
