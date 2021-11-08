904df0ceba15b4f0f01f482f370b692887168fb8
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-08 09:20:28 +0300

    Implement a database context structure

---
+++ data/src/context.rs
@@ -0,0 +1,30 @@
+use crate::diesel::Connection;
+use crate::repos::users_repo::UsersRepo;
+use crate::repos::DbConnection;
+use crate::result;
+
+pub struct DbContext<'a> {
+    db: &'a DbConnection,
+    users: UsersRepo<'a>,
+}
+
+impl<'a> DbContext<'a> {
+    pub fn new(db: &'a DbConnection) -> DbContext<'a> {
+        DbContext::<'a> {
+            db,
+            users: UsersRepo::<'a>::new(db),
+        }
+    }
+
+    pub fn connection(&self) -> &'a DbConnection {
+        self.db
+    }
+
+    pub fn users(&self) -> &UsersRepo<'a> {
+        &self.users
+    }
+}
+
+fn create_connection(url: &str) -> result::Result<DbConnection> {
+    Ok(DbConnection::establish(&url)?)
+}
--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod context;
 pub mod models;
 pub mod repos;
 pub mod result;
