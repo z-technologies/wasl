b0b17a0db477fc8bb3fa75dbf9bdab524bd109b5
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-08 09:53:49 +0300

    Rename InternalError to DatabaseError

--- data/src/result.rs
+++ data/src/result.rs
@@ -2,9 +2,8 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum DataError {
-    InternalError(diesel::result::Error),
+    DatabaseError(diesel::result::Error),
     ConnectionError(diesel::result::ConnectionError),
-    NotFound,
 }
 
 pub type Result<T> = std::result::Result<T, DataError>;
@@ -12,16 +11,15 @@ pub type Result<T> = std::result::Result<T, DataError>;
 impl fmt::Display for DataError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            DataError::InternalError(err) => write!(f, "{}", err),
-            DataError::ConnectionError(err) => write!(f, "{}", err),
-            DataError::NotFound => write!(f, "no such item"),
+            DataError::DatabaseError(err) => write!(f, "Database Error: {}", err),
+            DataError::ConnectionError(err) => write!(f, "Connection Error: {}", err),
         }
     }
 }
 
 impl From<diesel::result::Error> for DataError {
     fn from(err: diesel::result::Error) -> DataError {
-        DataError::InternalError(err)
+        DataError::DatabaseError(err)
     }
 }
 
