c8f1c2e2acf858b30a629270163806e18cd8ed5b
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-09 04:35:31 +0300

    Use the derive macro the implement the users repository

--- data/src/repos/users_repo.rs
+++ data/src/repos/users_repo.rs
@@ -1,52 +1,17 @@
+use data_derive;
+
 use crate::models::user::{NewUser, User};
-use crate::models::KeyType;
-use crate::repos::{DbConnection, Repo};
+use crate::repos::{DbConnection, Repo, RepoTypes};
 use crate::result;
-use crate::result::DataError;
 
-use diesel::ExpressionMethods;
-use diesel::QueryDsl;
-use diesel::RunQueryDsl;
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
 
+#[derive(data_derive::Repository)]
 pub struct UsersRepo<'a> {
-    db: &'a DbConnection,
+    pub db: &'a DbConnection,
 }
 
-impl<'db> Repo<User, NewUser<'_>> for UsersRepo<'db> {
-    fn get_all(&self) -> result::Result<Vec<User>> {
-        use crate::schema::users::dsl::*;
-        Ok(users.load::<User>(self.db)?)
-    }
-
-    fn get(&self, key: KeyType) -> result::Result<User> {
-        use crate::schema::users::dsl::*;
-        Ok(users.filter(id.eq(key)).get_result(self.db)?)
-    }
-
-    fn insert<'a>(&self, item: &'a NewUser) -> result::Result<User> {
-        use crate::schema::users::dsl::*;
-        Ok(diesel::insert_into(users)
-            .values(item)
-            .get_result::<User>(self.db)?)
-    }
-
-    fn update<'a>(&self, item: &'a User) -> result::Result<&'a User> {
-        use crate::schema::users::dsl::*;
-        diesel::update(users).set(item).execute(self.db)?;
-        Ok(item)
-    }
-
-    fn delete(&self, item: &User) -> result::Result<()> {
-        use crate::schema::users::dsl::*;
-        match diesel::delete(users.filter(id.eq(item.id))).execute(self.db) {
-            Ok(_) => Ok(()),
-            Err(err) => Err(DataError::from(err)),
-        }
-    }
-}
-
-impl<'a> UsersRepo<'a> {
-    pub fn new(db: &'a DbConnection) -> UsersRepo<'a> {
-        UsersRepo::<'a> { db }
-    }
+impl<'a> RepoTypes for UsersRepo<'a> {
+    type Model = User;
+    type InsertModel = NewUser<'a>;
 }
