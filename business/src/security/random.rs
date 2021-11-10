ee0feabb52a457ea06ce90cf62045467a07399a1
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 02:26:23 +0300

    Implement utility random methods

---
+++ business/src/security/random.rs
@@ -0,0 +1,23 @@
+use rand::prelude::*;
+
+struct Ascii;
+
+impl Distribution<char> for Ascii {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> char {
+        *b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()[]{}:;"
+            .choose(rng)
+            .unwrap() as char
+    }
+}
+
+pub fn generate_random_bytes<'a>(buf: &mut [u8]) {
+    thread_rng().fill_bytes(buf);
+}
+
+pub fn generate_random_string<D>(len: usize, dist: D) -> String {
+    thread_rng()
+        .sample_iter(&Ascii)
+        .take(len)
+        .map(char::from)
+        .collect()
+}
