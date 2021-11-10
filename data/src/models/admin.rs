3e4051082f9929dfcac90552fe5a03bfadcb90e6
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 01:40:17 +0300

    Add a model to represent the added many-to-many relationship

--- data/src/models/admin.rs
+++ data/src/models/admin.rs
@@ -1,7 +1,18 @@
 use crate::models::KeyType;
-use crate::schema::admins;
+use crate::schema::{admin_groups, admins};
+
+use crate::models::group::Group;
 
 use serde::{Deserialize, Serialize};
+
+#[derive(Identifiable, Queryable, Associations)]
+#[belongs_to(Admin)]
+#[belongs_to(Group)]
+pub struct AdminGroup {
+    pub id: KeyType,
+    pub admin_id: KeyType,
+    pub group_id: KeyType,
+}
 
 #[derive(Clone, Debug, Identifiable, Queryable, AsChangeset)]
 pub struct Admin {
