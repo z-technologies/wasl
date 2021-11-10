4046d04ab53a302c635c3fbcde25846f3c945919
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 01:44:35 +0300

    Implement a method to get groups for an admin on the admins repository

--- data/src/repos/admins_repo.rs
+++ data/src/repos/admins_repo.rs
@@ -1,10 +1,13 @@
 use data_derive;
 
 use crate::models::admin::*;
+use crate::models::group::*;
+
 use crate::repos::{DbConnection, Repo, RepoTypes};
 use crate::result;
 
-use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
+use diesel::pg::expression::dsl::any;
+use diesel::prelude::*;
 
 #[derive(data_derive::Repository)]
 pub struct AdminsRepo<'a> {
@@ -14,4 +17,17 @@ pub struct AdminsRepo<'a> {
 impl<'a> RepoTypes for AdminsRepo<'a> {
     type Model = Admin;
     type InsertModel = NewAdmin<'a>;
+}
+
+impl<'a> AdminsRepo<'a> {
+    pub fn get_admin_groups(&self, admin: &Admin) -> result::Result<Vec<Group>> {
+        use crate::schema::admin_groups::dsl::*;
+        use crate::schema::groups;
+
+        let group_ids = AdminGroup::belonging_to(admin).select(group_id);
+
+        Ok(groups::table
+            .filter(groups::id.eq(any(group_ids)))
+            .load::<Group>(self.db)?)
+    }
 }
