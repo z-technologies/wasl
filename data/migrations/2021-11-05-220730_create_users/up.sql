f01aeaac3872ace72a14b5109941e25cfbc66cf5
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 02:48:19 +0300

    Make username and email fields in the users table unique

--- data/migrations/2021-11-05-220730_create_users/up.sql
+++ data/migrations/2021-11-05-220730_create_users/up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
     id            SERIAL       PRIMARY KEY,
 
-    username      VARCHAR(32)  NOT NULL,
+    username      VARCHAR(32)  NOT NULL UNIQUE,
     password_hash VARCHAR(64)  NULL,
     password_salt VARCHAR(64)  NULL,
 
-    email         VARCHAR(64)  NOT NULL,
+    email         VARCHAR(64)  NOT NULL UNIQUE,
     is_active     BOOLEAN      NOT NULL DEFAULT FALSE,
 
     first_name    VARCHAR(32)  NULL,
