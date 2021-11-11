e98a9cd9ffeee9c6a4c124cd462bcc03d2358ddf
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 12:39:44 +0300

    Reorder externl crates

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,13 +1,15 @@
-extern crate data_derive;
-
 pub mod context;
 pub mod models;
 pub mod repos;
 pub mod result;
 pub mod schema;
 
+extern crate data_derive;
+
 extern crate serde;
+extern crate serde_json;
+
 #[macro_use]
 extern crate diesel;
-#[macro_use]
+
 extern crate derive_more;
