42e5d275c31c222236e4c03735ee51c3b9e3f2e9
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 08:15:57 +0300

    Use derive_more macros to implement From and Display traits on the crate error type

--- business/src/result.rs
+++ business/src/result.rs
@@ -1,24 +1,15 @@
-use std::fmt;
+use derive_more::{Display, From};
 
-#[derive(Debug)]
+#[derive(Debug, Display, From)]
 pub enum BusinessError {
+    #[display(fmt = "Data Error: {}", _0)]
     DataError(data::result::DataError),
+
+    #[display(fmt = "invalid password")]
     InvalidPassword,
+
+    #[display(fmt = "no password is set for the user")]
     NoPasswordIsSet,
 }
 
 pub type Result<T> = std::result::Result<T, BusinessError>;
-
-impl fmt::Display for BusinessError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            err => write!(f, "{:?}", err),
-        }
-    }
-}
-
-impl From<data::result::DataError> for BusinessError {
-    fn from(err: data::result::DataError) -> BusinessError {
-        BusinessError::DataError(err)
-    }
-}
