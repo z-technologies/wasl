361e6dbfdc916e93ecc1e42fa07944d08684c979
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-11 08:12:20 +0300

    Use derive_more macros to implement From and Display traits on the crate error type

--- data/src/result.rs
+++ data/src/result.rs
@@ -1,36 +1,15 @@
-use std::fmt;
+use derive_more::{Display, From};
 
-#[derive(Debug)]
+#[derive(Debug, Display, From)]
 pub enum DataError {
+    #[display(fmt = "Database Error: {}", _0)]
     DatabaseError(diesel::result::Error),
+
+    #[display(fmt = "Connection Error: {}", _0)]
     ConnectionError(diesel::result::ConnectionError),
+
+    #[display(fmt = "Environment Error: {}", _0)]
     EnvironmentError(std::env::VarError),
 }
 
 pub type Result<T> = std::result::Result<T, DataError>;
-
-impl fmt::Display for DataError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            err => write!(f, "{:?}", err),
-        }
-    }
-}
-
-impl From<diesel::result::Error> for DataError {
-    fn from(err: diesel::result::Error) -> DataError {
-        DataError::DatabaseError(err)
-    }
-}
-
-impl From<diesel::result::ConnectionError> for DataError {
-    fn from(err: diesel::result::ConnectionError) -> DataError {
-        DataError::ConnectionError(err)
-    }
-}
-
-impl From<std::env::VarError> for DataError {
-    fn from(err: std::env::VarError) -> DataError {
-        DataError::EnvironmentError(err)
-    }
-}
