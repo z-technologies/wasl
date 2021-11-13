3125408d57f0c0f69636664c009c69d7be71a099
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-14 01:06:49 +0300

    Add dependencies lazy_static, validator, and regex crates

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,15 +1,19 @@
+extern crate data_derive;
+
 pub mod context;
 pub mod models;
 pub mod repos;
 pub mod result;
 pub mod schema;
 
-extern crate data_derive;
-
-extern crate serde;
-extern crate serde_json;
-
+extern crate derive_more;
 #[macro_use]
 extern crate diesel;
 
-extern crate derive_more;
+extern crate regex;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate validator;
