b2cb3893ba71ae2cf67dc23181634309d94f2512
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-14 08:26:04 +0300

    Rewrite the users table accodring to the new ERD

--- data/migrations/2021-11-05-220730_create_users/up.sql
+++ data/migrations/2021-11-05-220730_create_users/up.sql
@@ -1,16 +1,15 @@
 CREATE TABLE users (
-    id            SERIAL        PRIMARY KEY,
+    id             SERIAL        PRIMARY KEY,
 
-    username      VARCHAR(32)   NOT NULL UNIQUE,
-    email         VARCHAR(64)   NOT NULL UNIQUE,
-    password_hash VARCHAR(128)  NULL,
+    username       VARCHAR(32)   NOT NULL UNIQUE,
+    email          VARCHAR(64)   NOT NULL UNIQUE,
+    password_hash  VARCHAR(128)  NULL,
+    is_active      BOOLEAN       NOT NULL DEFAULT FALSE,
 
-    first_name    VARCHAR(32)   NULL,
-    last_name     VARCHAR(32)   NULL,
-    profile_photo VARCHAR(256)  NULL,
+    first_name     VARCHAR(32)   NULL,
+    last_name      VARCHAR(32)   NULL,
+    profile_photo  VARCHAR(256)  NULL,
 
-    is_provider   BOOLEAN       NOT NULL,
-    is_active     BOOLEAN       NOT NULL DEFAULT FALSE,
-
-    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    cached_balance FLOAT8        NOT NULL DEFAULT 0,
+    created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
