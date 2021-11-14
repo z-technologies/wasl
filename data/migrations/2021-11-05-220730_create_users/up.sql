18967eddc9eb84a73bcdec9c83ea4efe6af8e693
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-14 05:50:01 +0300

    Rewrite the SQL of the users table

--- data/migrations/2021-11-05-220730_create_users/up.sql
+++ data/migrations/2021-11-05-220730_create_users/up.sql
@@ -1,14 +1,16 @@
 CREATE TABLE users (
-    id            SERIAL       PRIMARY KEY,
+    id            SERIAL        PRIMARY KEY,
 
-    username      VARCHAR(32)  NOT NULL UNIQUE,
-    password_hash VARCHAR(64)  NULL,
-    password_salt VARCHAR(64)  NULL,
+    username      VARCHAR(32)   NOT NULL UNIQUE,
+    email         VARCHAR(64)   NOT NULL UNIQUE,
+    password_hash VARCHAR(128)  NULL,
 
-    email         VARCHAR(64)  NOT NULL UNIQUE,
-    is_active     BOOLEAN      NOT NULL DEFAULT FALSE,
+    first_name    VARCHAR(32)   NULL,
+    last_name     VARCHAR(32)   NULL,
+    profile_photo VARCHAR(256)  NULL,
 
-    first_name    VARCHAR(32)  NULL,
-    last_name     VARCHAR(32)  NULL,
-    profile_photo VARCHAR(256) NULL
+    is_provider   BOOLEAN       NOT NULL,
+    is_active     BOOLEAN       NOT NULL DEFAULT FALSE,
+
+    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
