e7854aad8338db6757684ef37bea561e5514434c
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-14 08:39:12 +0300

    Move Repo traits and its dependencies to a separate module

--- data/src/repos/mod.rs
+++ data/src/repos/mod.rs
@@ -1,33 +1,7 @@
 pub mod groups_repo;
+pub mod repo;
 pub mod users_repo;
 
-use crate::models::KeyType;
-use crate::result;
-
-use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
-use diesel::PgConnection;
-
-pub type DbConnection = PgConnection;
-pub type DbConnectionManager = ConnectionManager<DbConnection>;
-
-pub type DbPooledConnection = PooledConnection<DbConnectionManager>;
-pub type DbPool = Pool<DbConnectionManager>;
-
-pub trait RepoTypes {
-    type Model;
-    type InsertModel;
-}
-
-pub trait Repo: RepoTypes {
-    fn get_all(&self) -> result::Result<Vec<Self::Model>>;
-    fn get(&self, id: KeyType) -> result::Result<Self::Model>;
-
-    fn insert(&self, item: &Self::InsertModel) -> result::Result<Self::Model>;
-    fn update<'a>(
-        &self,
-        item: &'a Self::Model,
-    ) -> result::Result<&'a Self::Model>;
-
-    fn delete(&self, item: &Self::Model) -> result::Result<()>;
-    fn get_connection(&self) -> result::Result<DbPooledConnection>;
-}
+pub use groups_repo::*;
+pub use repo::*;
+pub use users_repo::*;
---
+++ data/src/repos/repo.rs
@@ -0,0 +1,30 @@
+use crate::models::KeyType;
+use crate::result;
+
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use diesel::PgConnection;
+
+pub type DbConnection = PgConnection;
+pub type DbConnectionManager = ConnectionManager<DbConnection>;
+
+pub type DbPooledConnection = PooledConnection<DbConnectionManager>;
+pub type DbPool = Pool<DbConnectionManager>;
+
+pub trait RepoTypes {
+    type Model;
+    type InsertModel;
+}
+
+pub trait Repo: RepoTypes {
+    fn get_all(&self) -> result::Result<Vec<Self::Model>>;
+    fn get(&self, id: KeyType) -> result::Result<Self::Model>;
+
+    fn insert(&self, item: &Self::InsertModel) -> result::Result<Self::Model>;
+    fn update<'a>(
+        &self,
+        item: &'a Self::Model,
+    ) -> result::Result<&'a Self::Model>;
+
+    fn delete(&self, item: &Self::Model) -> result::Result<()>;
+    fn get_connection(&self) -> result::Result<DbPooledConnection>;
+}
