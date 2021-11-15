319fcf89c7e89addcbc9ae991e2f695c00b1c7e5
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-15 03:47:46 +0300

    Implement std::error::Error on error types of this crate

--- business/src/result.rs
+++ business/src/result.rs
@@ -2,6 +2,8 @@ use data::result::DataError;
 
 use derive_more::{Display, From};
 
+use std::error;
+
 #[derive(Debug, Display, From)]
 pub enum InternalError {
     #[display(fmt = "data error: {}", _0)]
@@ -13,3 +15,6 @@ pub enum UserError {
     #[display(fmt = "internal error: {}", _0)]
     InternalError(InternalError),
 }
+
+impl error::Error for InternalError {}
+impl error::Error for UserError {}
