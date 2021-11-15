8bd42665bd472b24e9021824abcaf1a58035cb6d
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-15 03:46:34 +0300

    Use derive_more derive macros on error types of this crate

--- business/src/result.rs
+++ business/src/result.rs
@@ -1,9 +1,15 @@
 use data::result::DataError;
 
+use derive_more::{Display, From};
+
+#[derive(Debug, Display, From)]
 pub enum InternalError {
+    #[display(fmt = "data error: {}", _0)]
     DataError(DataError),
 }
 
+#[derive(Debug, Display, From)]
 pub enum UserError {
+    #[display(fmt = "internal error: {}", _0)]
     InternalError(InternalError),
 }
