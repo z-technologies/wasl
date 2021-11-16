88c69062d1ba550e5931c15b6509b6ead7de5417
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-16 17:53:59 +0300

    Reimplement error conversion

--- api/src/result.rs
+++ api/src/result.rs
@@ -1,26 +1,22 @@
 use business::result::{InternalError, UserError};
 
-use actix_web::dev::HttpResponseBuilder;
-use actix_web::http::{header, StatusCode};
+use actix_web::http::StatusCode;
 use actix_web::{HttpResponse, ResponseError};
 use derive_more::{Display, From};
 use validator::ValidationErrors;
 
 #[derive(Debug, Display, From)]
 pub enum ApiError {
-    #[display(fmt = "internal error")]
+    #[display(fmt = "internal error: {}", _0)]
     InternalError(InternalError),
 
     #[display(fmt = "user error: {}", _0)]
     UserError(UserError),
 
-    #[display(fmt = "An internal error occurred. Please try again later.")]
-    InternalSechulingError,
+    #[display(fmt = "scheduling error")]
+    SechulingError,
 
-    #[display(fmt = "An internal error occurred. Please try again later.")]
-    InternalEnvironmentError(std::env::VarError),
-
-    #[display(fmt = "Validation error on field: {:?}", _0)]
+    #[display(fmt = "validation error on field(s): {}", _0)]
     ValidationError(ValidationErrors),
 }
 
@@ -28,21 +24,38 @@ pub type ApiResult<T> = std::result::Result<T, ApiError>;
 
 impl ResponseError for ApiError {
     fn error_response(&self) -> HttpResponse {
-        HttpResponseBuilder::new(self.status_code())
-            .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
-            .body(self.to_string())
+        if self.status_code() == StatusCode::INTERNAL_SERVER_ERROR {
+            HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR).finish()
+        } else {
+            HttpResponse::build(self.status_code()).body(self.to_string())
+        }
     }
 
     fn status_code(&self) -> StatusCode {
-        match *self {
+        match self {
+            ApiError::InternalError(_) | ApiError::SechulingError => {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+
             ApiError::ValidationError { .. } => StatusCode::BAD_REQUEST,
-            _ => StatusCode::INTERNAL_SERVER_ERROR,
+
+            ApiError::UserError(err) => match err {
+                UserError::InternalError(..) => {
+                    StatusCode::INTERNAL_SERVER_ERROR
+                }
+                UserError::PasswordNotSet => StatusCode::FORBIDDEN,
+                UserError::InvalidUsernameOrPassword => {
+                    StatusCode::UNAUTHORIZED
+                }
+                UserError::UsernameAlreadyInUse
+                | UserError::EmailAlreadyInUse => StatusCode::CONFLICT,
+            },
         }
     }
 }
 
 impl<E: std::fmt::Debug> From<actix_web::error::BlockingError<E>> for ApiError {
     fn from(_: actix_web::error::BlockingError<E>) -> Self {
-        ApiError::InternalSechulingError
+        ApiError::SechulingError
     }
 }
