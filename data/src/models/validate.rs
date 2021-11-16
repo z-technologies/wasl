40c24b683a6a998fef0fda5db52bd1591977a5f7
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-16 18:39:03 +0300

    Add a pattern to validate group names (using java package style)

--- data/src/models/validate.rs
+++ data/src/models/validate.rs
@@ -3,4 +3,6 @@ use regex::Regex;
 lazy_static! {
     pub static ref RE_USERNAME: Regex =
         Regex::new(r"^[a-z][a-z0-9_]{3,31}$").unwrap();
+    pub static ref RE_PACKAGE: Regex =
+        Regex::new(r"^[a-z][a-z0-9_]*(\.[a-z0-9_]+)+[0-9a-z_]$").unwrap();
 }
