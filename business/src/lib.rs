035068f151bf828d5a41bda6c85aadf2ae4fedc2
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-17 15:02:57 +0300

    Add dependencies serde, native-tls, and lettre crates

--- business/src/lib.rs
+++ business/src/lib.rs
@@ -1,7 +1,10 @@
 extern crate argon2;
 extern crate base64;
 extern crate derive_more;
+extern crate lettre;
+extern crate native_tls;
 extern crate rand;
+extern crate serde;
 
 pub mod io;
 pub mod result;
