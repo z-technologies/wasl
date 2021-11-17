157fb3dd417c8a7dcc01023f9ceaef8bd073ff3e
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-17 18:55:42 +0300

    Export the email managment service

--- api/src/main.rs
+++ api/src/main.rs
@@ -14,8 +14,8 @@ mod settings;
 mod setup;
 
 use crate::settings::Settings;
-use business::services::AuthSerivce;
 
+use business::services::{AuthSerivce, EmailService};
 use data::context::{create_connection_pool, DbContext};
 
 use actix_web::{App, HttpServer};
@@ -29,21 +29,32 @@ async fn main() -> std::io::Result<()> {
 
     let settings: Arc<Settings> = Arc::new(Settings::new().unwrap());
 
+    let ctx = create_db_ctx(&settings);
     let listen_ep = settings.server.endpoint();
-    let db_url = settings.database.url();
-    let db_pool = create_connection_pool(&db_url, MAX_POOL_CONNECTIONS)
-        .expect("could not create a database pool");
-
-    let ctx = DbContext::new(db_pool);
 
     HttpServer::new(move || {
         App::new()
             .data(settings.clone())
             .data(ctx.clone())
-            .data(AuthSerivce { ctx: ctx.clone() })
+            .data(AuthSerivce::new(ctx.clone()))
+            .data(EmailService::new(
+                &settings.email.smtp_host,
+                settings.email.smtp_port,
+                &settings.email.smtp_username,
+                &settings.email.smtp_password,
+                settings.email.require_tls,
+            ))
             .configure(setup::setup_handlers)
     })
     .bind(listen_ep)?
     .run()
     .await
+}
+
+fn create_db_ctx(settings: &Settings) -> DbContext {
+    let db_url = settings.database.url();
+    let db_pool = create_connection_pool(&db_url, MAX_POOL_CONNECTIONS)
+        .expect("could not create a database pool");
+
+    DbContext::new(db_pool)
 }
