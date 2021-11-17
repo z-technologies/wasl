7a31952a8e6d48aef3a782b245702f3625b71710
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-17 20:15:54 +0300

    Fix whitespace

--- data/migrations/2021-11-10-221633_create_user_groups_table/up.sql
+++ data/migrations/2021-11-10-221633_create_user_groups_table/up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE user_groups (
-    id       SERIAL PRIMARY KEY,
-
-    user_id  SERIAL NOT NULL    REFERENCES users(id),
-    group_id SERIAL NOT NULL    REFERENCES groups(id)
+    id       SERIAL          PRIMARY KEY,
+    user_id  SERIAL NOT NULL REFERENCES users(id),
+    group_id SERIAL NOT NULL REFERENCES groups(id)
 )
