30e2b072409a7b650ff33f97e9a9f868e831c250
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-17 18:57:10 +0300

    Use individual values instead of config struct

--- business/src/services/email.rs
+++ business/src/services/email.rs
@@ -4,39 +4,35 @@ use lettre::message::Mailbox;
 use lettre::transport::smtp::client::{Tls, TlsParameters};
 use lettre::transport::smtp::response::Response;
 use lettre::{Message, SmtpTransport, Transport};
-use serde::Deserialize;
 
 pub struct EmailService {
     mailer: SmtpTransport,
 }
 
 impl EmailService {
-    pub fn new(settings: &EmailSettings) -> Result<EmailService> {
-        let builder = SmtpTransport::relay(&settings.smtp_host)?
-            .port(settings.smtp_port)
-            .credentials(
-                (
-                    settings.smtp_username.clone(),
-                    settings.smtp_password.clone(),
-                )
-                    .into(),
-            );
+    pub fn new(
+        host: &str,
+        port: u16,
+        username: &str,
+        password: &str,
+        tls: bool,
+    ) -> Result<EmailService> {
+        let mut builder = SmtpTransport::relay(host)?
+            .port(port)
+            .credentials((username.to_owned(), password.to_owned()).into());
+
+        if tls {
+            builder = builder.tls(Tls::Required(
+                TlsParameters::builder(host.into()).build_native()?,
+            ));
+        }
 
         Ok(EmailService {
-            mailer: if settings.enable_tls {
-                builder
-                    .tls(Tls::Required(
-                        TlsParameters::builder(settings.smtp_host.clone())
-                            .build_native()?,
-                    ))
-                    .build()
-            } else {
-                builder.build()
-            },
+            mailer: builder.build(),
         })
     }
 
-    pub fn send_email<'a>(
+    pub fn send<'a>(
         &self,
         subject: &'a str,
         body: &'a str,
@@ -56,15 +52,4 @@ impl EmailService {
 
 pub fn make_mail_box<'a>(name: &'a str, email: &'a str) -> Result<Mailbox> {
     Ok(format!("{} <{}>", name, email).parse()?)
-}
-
-#[derive(Debug, Deserialize)]
-pub struct EmailSettings {
-    pub smtp_host: String,
-    pub smtp_port: u16,
-    pub smtp_username: String,
-    pub smtp_password: String,
-
-    pub noreply_address: String,
-    pub enable_tls: bool,
 }
