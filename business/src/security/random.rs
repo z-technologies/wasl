b5f3fd166f52e011a964c1b78c0e144cb4b97091
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-23 14:22:18 +0300

    Add a funciton to generate random strings

--- business/src/security/random.rs
+++ business/src/security/random.rs
@@ -1,5 +1,14 @@
+use rand::distributions::Alphanumeric;
 use rand::prelude::*;
 
 pub fn generate_random_bytes<'a>(buf: &mut [u8]) {
     thread_rng().fill_bytes(buf);
+}
+
+pub fn generate_alphanum_string<const LEN: usize>() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(LEN)
+        .map(char::from)
+        .collect()
 }
