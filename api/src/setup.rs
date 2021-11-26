c5acc24289d650b83aa188681c94b6cf84c05a55
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-26 03:44:37 +0300

    Use the max pool connections value from the settings object

--- api/src/setup.rs
+++ api/src/setup.rs
@@ -1,17 +1,13 @@
-use crate::handlers::auth;
-use crate::handlers::echo;
+use crate::handlers::{auth, echo};
 use crate::settings::Settings;
-use crate::Arc;
-use business::services::AuthSerivce;
-use business::services::EmailService;
-use data::context::create_connection_pool;
-use data::context::DbContext;
+
+use business::services::{AuthSerivce, EmailService};
+use data::context::{create_connection_pool, DbContext};
 
 use actix_web::web;
 
 use std::env;
-
-const MAX_POOL_CONNECTIONS: u32 = 4;
+use std::sync::Arc;
 
 pub fn setup_handlers(cfg: &mut web::ServiceConfig) {
     cfg.service({
@@ -29,10 +25,12 @@ pub fn setup_handlers(cfg: &mut web::ServiceConfig) {
 
 pub fn setup_data(cfg: &mut web::ServiceConfig, settings: Arc<Settings>) {
     // create database context
-    let db_url = settings.database.url();
-    let db_pool = create_connection_pool(&db_url, MAX_POOL_CONNECTIONS)
-        .expect("could not create a database pool");
-    let ctx = DbContext::new(db_pool);
+    let pool = create_connection_pool(
+        &settings.database.url(),
+        settings.database.max_pool_connections,
+    )
+    .expect("could not create a database pool");
+    let ctx = DbContext::new(pool);
 
     // create services
     let email_svc = Arc::new(EmailService::new(&settings.email).unwrap());
