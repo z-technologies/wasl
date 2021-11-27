ad2e6d903917a449c1b768853e908627ee3d60dc
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-27 19:30:24 +0300

    Return the crate result instead of `impl Responder`

--- api/src/handlers/echo.rs
+++ api/src/handlers/echo.rs
@@ -1,6 +1,9 @@
-use actix_web::{get, web, Responder};
+use crate::result::Result;
+
+use actix_web::HttpResponse;
+use actix_web::{get, web};
 
 #[get("/echo/{data}")]
-pub async fn echo(web::Path(data): web::Path<String>) -> impl Responder {
-    data
+pub async fn echo(web::Path(data): web::Path<String>) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body(data))
 }
