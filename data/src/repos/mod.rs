8c6f7d1018bbbba7e9953f4d2d72732c1a2ecbcb
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-28 18:36:39 +0300

    Export repo submodules

--- data/src/repos/mod.rs
+++ data/src/repos/mod.rs
@@ -3,8 +3,12 @@ pub use repo::*;
 
 pub mod confirmations_repo;
 pub mod groups_repo;
+pub mod products_repo;
+pub mod services_repo;
 pub mod users_repo;
 
 pub use confirmations_repo::*;
 pub use groups_repo::*;
+pub use products_repo::*;
+pub use services_repo::*;
 pub use users_repo::*;
