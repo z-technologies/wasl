a4313493d51aeb11c32cd8d1fbeae41404d4fd0c
Ayman Al-Qadhi <alqd@protonmail.com>
2021-11-28 18:36:29 +0300

    Add a repository for the added model NewProduct

---
+++ data/src/repos/services_repo.rs
@@ -0,0 +1,14 @@
+use crate::models::service::*;
+use crate::repos::DbPool;
+use crate::result::{DataError, Result};
+
+use data_derive::Repository;
+use diesel::prelude::*;
+
+#[derive(Clone, Repository)]
+#[repo_table_name = "services"]
+#[repo_model = "Service"]
+#[repo_insert_model = "NewService"]
+pub struct ProductsRepo {
+    pub pool: DbPool,
+}
