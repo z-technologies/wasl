fbd6abf4e21c71219a981caa840b714873913b08
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-08 05:21:16 +0300

    Export ConfirmationsService at module level

--- business/src/services/mod.rs
+++ business/src/services/mod.rs
@@ -1,9 +1,11 @@
 pub mod auth;
+pub mod confirmations;
 pub mod email;
 pub mod services;
 pub mod users;
 
 pub use auth::AuthSerivce;
+pub use confirmations::ConfirmationsService;
 pub use email::EmailService;
 pub use services::ServicesService;
 pub use users::UsersService;
