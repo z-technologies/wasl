c0c9d58fc61c124f3e009f1d916f8768f7b361a8
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-09 01:36:26 +0300

    Move dependencies from data to business crate

--- business/src/lib.rs
+++ business/src/lib.rs
@@ -3,9 +3,15 @@ extern crate base64;
 extern crate derive_more;
 extern crate lettre;
 extern crate native_tls;
+#[macro_use]
+extern crate lazy_static;
 extern crate rand;
+extern crate regex;
 extern crate serde;
+extern crate serde_json;
+extern crate validator;
 
+pub mod data;
 pub mod io;
 pub mod result;
 pub mod security;
