668d5d5a733c048e1ac006eb5ef88ab31848ca35
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-15 09:04:49 +0300

    Use a strong-typed enum for the state field in transactions table

--- wasl/migrations/2021-12-11-014306_create_transactions/up.sql
+++ wasl/migrations/2021-12-11-014306_create_transactions/up.sql
@@ -1,10 +1,15 @@
-CREATE TABLE transactions (
-    id          SERIAL    PRIMARY KEY,
-    amount      NUMERIC   NOT NULL,
-    state       BOOLEAN   NULL,
-    signature   VARCHAR   NOT NULL,
+CREATE TYPE transaction_state AS ENUM(
+    'pending',
+    'declined',
+    'confirmed'
+);
 
-    sender      SERIAL    NOT NULL REFERENCES users(id),
-    receiver    SERIAL    NOT NULL REFERENCES users(id),
+CREATE TABLE transactions (
+    id          SERIAL                   PRIMARY KEY,
+    amount      NUMERIC                  NOT NULL,
+    state       transaction_state        NOT NULL DEFAULT 'pending',
+    signature   VARCHAR                  NOT NULL,
+    sender      SERIAL                   NOT NULL REFERENCES users(id),
+    receiver    SERIAL                   NOT NULL REFERENCES users(id),
     made_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
