8b687b61ef3bd1dd82b04bbce43bc1a586eca99e
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-15 07:04:45 +0300

    Update models module imports

--- wasl/src/data/models/mod.rs
+++ wasl/src/data/models/mod.rs
@@ -1,15 +1,19 @@
 pub mod validate;
 
 pub mod confirmation;
+pub mod financial_record;
 pub mod group;
 pub mod product;
 pub mod service;
+pub mod transaction;
 pub mod user;
 
 pub use confirmation::*;
+pub use financial_record::*;
 pub use group::*;
 pub use product::*;
 pub use service::*;
+pub use transaction::*;
 pub use user::*;
 
 pub type KeyType = i32;
