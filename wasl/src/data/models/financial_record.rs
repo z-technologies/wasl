faf3ee35669bf81a53ef62ba0c94247b9242d493
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-15 09:08:05 +0300

    Add a mapping enum for the created postgres financial_record_state enum

--- wasl/src/data/models/financial_record.rs
+++ wasl/src/data/models/financial_record.rs
@@ -10,9 +10,18 @@ pub struct FinancialRecord {
     pub id: KeyType,
 
     pub amount: BigDecimal,
-    pub state: Option<bool>,
+
+    pub state: FinancialRecordState,
     pub signature: String,
 
     pub made_by: KeyType,
     pub made_at: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Clone, Debug, DbEnum)]
+#[DieselType = "Financial_record_state"]
+pub enum FinancialRecordState {
+    Pending,
+    Verified,
+    Rejected,
 }
