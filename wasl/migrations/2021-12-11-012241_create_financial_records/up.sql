78daac4fe2752a5e453ef7411a02f51aa4bc50dc
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-16 11:06:28 +0300

    Remove signature field from financial_records table

--- wasl/migrations/2021-12-11-012241_create_financial_records/up.sql
+++ wasl/migrations/2021-12-11-012241_create_financial_records/up.sql
@@ -8,9 +8,7 @@ CREATE TABLE financial_records (
     id          SERIAL                   PRIMARY KEY,
     amount      NUMERIC                  NOT NULL,
     state       financial_record_state   NOT NULL DEFAULT 'pending',
-    signature   VARCHAR                  NOT NULL,
     made_by     SERIAL                   NOT NULL REFERENCES users(id),
     made_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    
     verified_by SERIAL                   REFERENCES users(id)
 );
