a9ce50ff726e3f8c0093c7ba71bddbc912f13a64
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-16 16:34:31 +0300

    Add reservations handling methods to services service

--- wasl/src/services/services.rs
+++ wasl/src/services/services.rs
@@ -1,5 +1,8 @@
 use crate::data::connection::*;
-use crate::data::models::{KeyType, NewService, Service, User};
+use crate::data::models::{
+    KeyType, NewService, NewServiceReservation, Service, ServiceReservation,
+    User,
+};
 use crate::result::{Result, UserError};
 
 use diesel::prelude::*;
@@ -34,4 +37,57 @@ impl ServicesService {
 
         Ok(diesel::delete(&service).execute(&self.conn.get()?)?)
     }
+
+    pub fn reservations(
+        &self,
+        service: &Service,
+    ) -> Result<Vec<ServiceReservation>> {
+        Ok(
+            ServiceReservation::belonging_to(service)
+                .load(&self.conn.get()?)?,
+        )
+    }
+
+    pub fn can_make_reservation(
+        &self,
+        service: &Service,
+        reservation: &NewServiceReservation,
+    ) -> Result<bool> {
+        let reservations = self.reservations(service)?;
+
+        Ok(reservations.iter().all(|r| {
+            !periods_overlap(
+                (&r.reservation_begin, &r.reservation_end),
+                (&reservation.reservation_begin, &reservation.reservation_end),
+            )
+        }))
+    }
+
+    pub fn make_reservation(
+        &self,
+        reservation: &NewServiceReservation,
+    ) -> Result<ServiceReservation> {
+        use crate::data::schema::service_reservations::dsl::*;
+        use crate::data::schema::services::dsl::*;
+
+        let service = services
+            .find(reservation.service_id)
+            .get_result(&self.conn.get()?)?;
+
+        if !self.can_make_reservation(&service, reservation)? {
+            return Err(UserError::TimePeriodsOverlap);
+        }
+
+        Ok(diesel::insert_into(service_reservations)
+            .values(reservation)
+            .get_result(&self.conn.get()?)?)
+    }
+}
+
+#[inline]
+fn periods_overlap(
+    lhs: (&chrono::NaiveDateTime, &chrono::NaiveDateTime),
+    rhs: (&chrono::NaiveDateTime, &chrono::NaiveDateTime),
+) -> bool {
+    lhs.0 < rhs.1 && rhs.0 < lhs.1
 }
