f2342a20845d418cdb961b9265c8c802b35e3ee6
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-16 11:06:40 +0300

    Remove signature field from transactions table

--- wasl/migrations/2021-12-11-014306_create_transactions/up.sql
+++ wasl/migrations/2021-12-11-014306_create_transactions/up.sql
@@ -8,7 +8,6 @@ CREATE TABLE transactions (
     id          SERIAL                   PRIMARY KEY,
     amount      NUMERIC                  NOT NULL,
     state       transaction_state        NOT NULL DEFAULT 'pending',
-    signature   VARCHAR                  NOT NULL,
     sender      SERIAL                   NOT NULL REFERENCES users(id),
     receiver    SERIAL                   NOT NULL REFERENCES users(id),
     made_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
