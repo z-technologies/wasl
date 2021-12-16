efccfc6c6067cd4a6b48265daebdba5e8fc97d08
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-16 16:33:38 +0300

    Add a model to map service_reservations table

--- wasl/src/data/models/service.rs
+++ wasl/src/data/models/service.rs
@@ -1,5 +1,5 @@
 use crate::data::models::{KeyType, User};
-use crate::data::schema::services;
+use crate::data::schema::{service_reservations, services};
 
 use serde::{Deserialize, Serialize};
 
@@ -32,4 +32,34 @@ pub struct NewService {
     pub available_begin: Option<chrono::NaiveTime>,
     pub available_end: Option<chrono::NaiveTime>,
     pub user_id: KeyType,
+}
+
+#[derive(
+    AsChangeset,
+    Associations,
+    Clone,
+    Debug,
+    Deserialize,
+    Identifiable,
+    Queryable,
+    Serialize,
+)]
+#[belongs_to(User, foreign_key = made_by)]
+#[belongs_to(Service)]
+pub struct ServiceReservation {
+    #[serde(skip)]
+    pub id: KeyType,
+    pub made_by: KeyType,
+    pub service_id: KeyType,
+    pub reservation_begin: chrono::NaiveDateTime,
+    pub reservation_end: chrono::NaiveDateTime,
+}
+
+#[derive(Debug, Insertable)]
+#[table_name = "service_reservations"]
+pub struct NewServiceReservation {
+    pub made_by: KeyType,
+    pub service_id: KeyType,
+    pub reservation_begin: chrono::NaiveDateTime,
+    pub reservation_end: chrono::NaiveDateTime,
 }
