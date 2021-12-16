3d7d6452d020e90b65d8684b3e78f610b16e73b5
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-16 12:04:35 +0300

    Fix whitespace

--- wasl/migrations/2021-12-16-082308_create_financial_record_verifications/up.sql
+++ wasl/migrations/2021-12-16-082308_create_financial_record_verifications/up.sql
@@ -4,8 +4,11 @@ CREATE TYPE financial_record_verification_outcome AS ENUM(
 );
 
 CREATE TABLE financial_record_verifications (
-    id SERIAL                                         PRIMARY KEY,
-    outcome     financial_record_verification_outcome NOT NULL,
+    id SERIAL PRIMARY KEY,
+
+    outcome financial_record_verification_outcome NOT NULL,
+    financial_record_id SERIAL NOT NULL REFERENCES financial_records(id),
+
     verified_by SERIAL                   NOT NULL REFERENCES users(id),
     verified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
