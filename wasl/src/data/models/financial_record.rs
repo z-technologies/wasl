3943c672d0365de24c7c9bfea5d201030e97aa75
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-16 12:05:37 +0300

    Update financial record model to relfect last changes

--- wasl/src/data/models/financial_record.rs
+++ wasl/src/data/models/financial_record.rs
@@ -1,23 +1,31 @@
 use crate::data::models::{KeyType, User};
-use crate::data::schema::financial_records;
+use crate::data::schema::{financial_record_verifications, financial_records};
 
 use bigdecimal::BigDecimal;
-use validator::Validate;
 
-#[derive(Associations, Clone, Debug, Identifiable, Queryable, Validate)]
+#[derive(Associations, Clone, Debug, Identifiable, Queryable)]
 #[belongs_to(User, foreign_key = "made_by")]
 pub struct FinancialRecord {
     pub id: KeyType,
     pub amount: BigDecimal,
-    pub state: FinancialRecordState,
     pub made_by: KeyType,
     pub made_at: chrono::DateTime<chrono::Utc>,
 }
 
+#[derive(Associations, Clone, Debug, Identifiable, Queryable)]
+#[belongs_to(User, foreign_key = "verified_by")]
+#[belongs_to(FinancialRecord)]
+pub struct FinancialRecordVerification {
+    pub id: KeyType,
+    pub outcome: FinancialRecordVerificationOutcome,
+    pub financial_record_id: KeyType,
+    pub verified_by: KeyType,
+    pub verified_at: chrono::DateTime<chrono::Utc>,
+}
+
 #[derive(Clone, Debug, DbEnum)]
-#[DieselType = "Financial_record_state"]
-pub enum FinancialRecordState {
-    Pending,
+#[DieselType = "Financial_record_verification_outcome"]
+pub enum FinancialRecordVerificationOutcome {
     Rejected,
     Verified,
 }
