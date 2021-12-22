b95f57565205199919c9146193f8a85ae9ca6dc6
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-22 08:11:30 +0300

    Remove user_id since it can be infered from the transaction

--- wasl/migrations/2021-12-16-115936_create_service_reservations/up.sql
+++ wasl/migrations/2021-12-16-115936_create_service_reservations/up.sql
@@ -4,7 +4,6 @@ CREATE TABLE service_reservations (
     reservation_begin TIMESTAMP WITH TIME ZONE NOT NULL,
     reservation_end   TIMESTAMP WITH TIME ZONE NOT NULL,
 
-    made_by        SERIAL NOT NULL REFERENCES users(id),
     service_id     SERIAL NOT NULL REFERENCES services(id),
     transaction_id SERIAL NOT NULL REFERENCES transactions(id)
 );
--- wasl/migrations/2021-12-18-134110_create_product_orders/up.sql
+++ wasl/migrations/2021-12-18-134110_create_product_orders/up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE product_orders (
     id             SERIAL PRIMARY KEY,
 
-    made_by        SERIAL NOT NULL REFERENCES users(id),
     product_id     SERIAL NOT NULL REFERENCES products(id),
     transaction_id SERIAL NOT NULL REFERENCES transactions(id)
 );
