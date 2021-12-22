055d1dca4b2e0c98de12357364860dfb9ad871c0
Ayman Al-Qadhi <alqd@protonmail.com>
2021-12-22 07:35:07 +0300

    Redorder columns

--- wasl/migrations/2021-11-17-164639_create_confirmations/up.sql
+++ wasl/migrations/2021-11-17-164639_create_confirmations/up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE confirmations (
     id         SERIAL    PRIMARY KEY,
-    user_id    SERIAL    NOT NULL REFERENCES users(id),
+
     otp        VARCHAR   NOT NULL,
     token      VARCHAR   NOT NULL,
+    user_id    SERIAL    NOT NULL REFERENCES users(id),
+
     issued_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL
 )
--- wasl/migrations/2021-12-16-115936_create_service_reservations/up.sql
+++ wasl/migrations/2021-12-16-115936_create_service_reservations/up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE service_reservations (
     id                SERIAL PRIMARY KEY,
-    made_by           SERIAL NOT NULL REFERENCES users(id),
-    service_id        SERIAL NOT NULL REFERENCES services(id),
 
     reservation_begin TIMESTAMP WITH TIME ZONE NOT NULL,
-    reservation_end   TIMESTAMP WITH TIME ZONE NOT NULL
+    reservation_end   TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    made_by        SERIAL NOT NULL REFERENCES users(id),
+    service_id     SERIAL NOT NULL REFERENCES services(id),
+    transaction_id SERIAL NOT NULL REFERENCES transactions(id)
 );
